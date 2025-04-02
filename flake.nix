c2096bea88f665ed59e26cefd546fd307794b7f9
Kett Lovahr <catusfelony@gmail.com>
2025-04-02 01:00:12 -0300

    Flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    description = "A very basic flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }: let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+        deps = with pkgs; [
+            pkg-config
+            clang
+            raylib
+
+            libGL
+            glfw
+            xorg.libX11
+            xorg.libX11.dev
+            xorg.libXcursor
+            xorg.libXinerama
+            xorg.libXrandr
+            xorg.libXi
+        ];
+    in {
+        devShells.${system}.default = pkgs.mkShell {
+            buildInputs = deps;
+
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath deps;
+        };
+    };
+}
